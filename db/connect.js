c019c09cfa8cad9c2cfa6bf31831a3793576b702
jkkitchen <jkitchen1@byupathway.edu>
2026-05-12 21:58:42 -0400

    Got ride of debugging code

--- db/connect.js
+++ db/connect.js
@@ -4,9 +4,6 @@ const mongoose = require('mongoose');
 const URL = process.env.MONGODB_URL;
 const connectDB = async () => {
     try {
-        //Debugging
-        console.log("MONGO URL:", process.env.MONGODB_URL)
-
         await mongoose.connect(URL);
         console.log('MongoDB Connected!');
     } catch (err) {
