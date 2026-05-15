d11e5a57d7dea1eb7d299e83491a364e7ac9fbb8
jkkitchen <jkitchen1@byupathway.edu>
2026-05-15 15:34:06 -0400

    Swagger Documentation Complete

--- server.js
+++ server.js
@@ -1,23 +1,27 @@
 //dotenv.config() loads variables in Node's global process.env objects automatically so you don't need to save this as a constant
 require('dotenv').config()
 const express = require('express');
-const bodyParser = require('body-parser');
 const { connectDB } = require('./db/connect');
 const app = express();
 const contactsRoute = require('./routes/contactsRoute')
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger-output.json');
 
 //Get port from .env file
 const port = process.env.PORT || 3000;
 
-//MUST PUT THIS BEFORE CONTACTS ROUTES OR POST WILL NOT WORK
+//MIDDLEWARE: MUST PUT THIS BEFORE CONTACTS ROUTES OR POST WILL NOT WORK
 //Rather than using bodyParser, use express.json to do the same thing
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); //tells Express to parse incoming form data so it can be accessed in req.body
 
-//Pull route to get/add/update/delete contacts
+//ROUTES
 app.use('/contacts', contactsRoute)
 
-//Connect to database and start server
+//SWAGGER: API DOCUMENTATION
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+//CONNECT TO DB AND START SERVER
 const startServer = async () => {
     try {
         //Connect to database
---
+++ swagger.js
@@ -0,0 +1,18 @@
+const swaggerAutogen = require('swagger-autogen')();
+
+const doc = {
+  info: {
+    title: 'CSE 341 Contacts API Documentation',
+    description: 'Week 2 Assignment'
+  },
+    host: 'localhost:3000',
+    schemes: ['http'],
+    basePath: '/contacts'
+};
+
+const outputFile = './swagger-output.json';
+
+//Endpoint is server.js because routes are listed there
+const endpointsFiles = ['./server.js'];
+
+swaggerAutogen(outputFile, endpointsFiles, doc);
