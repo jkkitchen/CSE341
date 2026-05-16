7d8bcf63cdb047805bf10f2512f651e0eb8cf31d
jkkitchen <jkitchen1@byupathway.edu>
2026-05-16 13:00:42 -0400

    Added comment in put route

--- routes/contactsRoute.js
+++ routes/contactsRoute.js
@@ -18,7 +18,22 @@ contactsRouter.get('/:id', getSingleContact);
 contactsRouter.post('/', createContact);
 
 //PUT route to update a contact
-contactsRouter.put('/:id', updateContact);
+contactsRouter.put('/:id',
+    /* To ensure that the PUT route in api-docs has a body space for updates
+        #swagger.parameters['body'] = {
+            in: 'body',
+            description: 'Updated contact information',
+            required: true,
+            schema: {
+                firstName: 'Jessica',
+                lastName: 'Kitchen',
+                email: 'jessica@test.com',
+                favoriteColor: 'Purple',
+                birthday: '1995-01-01'
+            }
+        }
+    */
+    updateContact);
 
 //DELETE route to delete a contact
 contactsRouter.delete('/:id', deleteContact);
